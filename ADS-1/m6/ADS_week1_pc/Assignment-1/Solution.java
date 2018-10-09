25e823d60ace5e7d5ee2b44cdf319c1c8f85383e
sai hindu <hindunaini@gmail.com>
2018-10-08 18:09:24 -0700

    "m6-exam"

---
+++ ADS-1/m6/ADS_week1_pc/Assignment-1/Solution.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+class AddLargeNumbers {
+    
+    public static LinkedList numberToDigits(String number) {
+
+    }
+
+    public static String digitsToNumber(LinkedList list) {
+
+    }
+
+    public static LinkedList addLargeNumbers(LinkedList list1, LinkedList list2) {
+
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        String p = sc.nextLine();
+        String q = sc.nextLine();
+        switch(input){
+            case "numberToDigits":
+                LinkedList pDigits = AddLargeNumbers.numberToDigits(p);
+                LinkedList qDigits = AddLargeNumbers.numberToDigits(q);
+                System.out.println(AddLargeNumbers.digitsToNumber(pDigits));
+                System.out.println(AddLargeNumbers.digitsToNumber(qDigits));
+                break;
+
+            case "addLargeNumbers":
+                LinkedList pDigits = AddLargeNumbers.numberToDigits(p);
+                LinkedList qDigits = AddLargeNumbers.numberToDigits(q);
+                LinkedList result = AddLargeNumbers.addLargeNumbers(pDigits, qDigits);
+                System.out.println(AddLargeNumbers.digitsToNumber(result));
+                break;
+        }
+    }
+    
+}

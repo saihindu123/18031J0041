1f87a2bc19e35c7e29f07bb0ca4c9def5e646e07
sai <sai@example.com>
2018-10-31 17:55:30 -0700

    "check_style_score -> 2 of 5 passed. style score: 0/1 "

--- ADS-1/m12/13.1 isMinHeap/Solution.java
+++ ADS-1/m12/13.1 isMinHeap/Solution.java
@@ -1,150 +1,132 @@
 
 import java.util.Scanner;
 
-public class Solution
+class Heap
 {
-
-	public static void main(String[] args)
+	boolean  less(String a[],int j,int i)
 	{
-	Scanner s=new Scanner(System.in);
-	String s1=s.nextLine();
-	//Maxheap m1=new Maxheap();
-	switch(s1)
-	{
-	case "String":int n=s.nextInt();
-	              String s2[]=new String[n];
-	              String s01=s.nextLine();
-	              for(int i = 0;s.hasNext();i++)
-	              {
-	            	 String s3=s.next();
-	            
-	            	 String s4[]=null;
-	            	 s4=s3.split(",");
-	            	
-	            	 Minheap m=new Minheap();
-	            	System.out.println(m.sink(1,s4));
-	              }
-	              break;
-	case "Integer":
-		int n1=s.nextInt();
-        String s21[]=new String[n1];
-        String s011=s.nextLine();
-        
-        for(int i = 0;s.hasNext();i++)
-        {
-      	 String s3=s.next();
-      
-      	 String s4[]=null;
-      	 s4=s3.split(",");
-      	Double d[]=new Double[s4.length+1];
-      	for(int p=1;p<=s4.length;p++)
-      	{
-      		d[p]=Double.parseDouble(s4[p-1]);
-      	}
-      	 Minheap m=new Minheap();
-      	System.out.println(m.sink1(1,d));
-        }
-        break;
-	case "Double":
-		int n11=s.nextInt();
-        String s211[]=new String[n11];
-        String s0111=s.nextLine();
-        
-        for(int i = 0;s.hasNext();i++)
-        {
-      	 String s3=s.next();
-      
-      	 String s4[]=null;
-      	 s4=s3.split(",");
-      	Double d[]=new Double[s4.length+1];
-      	for(int p=1;p<=s4.length;p++)
-      	{
-      		d[p]=Double.parseDouble(s4[p-1]);
-      	}
-      	 Minheap m=new Minheap();
-      	System.out.println(m.sink1(1,d));
-        }
-        break;
-        
-	case "Float":
-		int n111=s.nextInt();
-        String s2111[]=new String[n111];
-        String s01111=s.nextLine();
-        
-        for(int i = 0;s.hasNext();i++)
-        {
-      	 String s3=s.next();
-      
-      	 String s4[]=null;
-      	 s4=s3.split(",");
-      	Double d[]=new Double[s4.length+1];
-      	for(int p=1;p<=s4.length;p++)
-      	{
-      		d[p]=Double.parseDouble(s4[p-1]);
-      	}
-      	 Minheap m=new Minheap();
-      	System.out.println(m.sink1(1,d));
-        }
-        break;
-	}
-
-	}
-
-}
-
-class Minheap
-{
-	
-	boolean sink(int i,String[] a)
-	{
-		boolean z = false;
-		int n=a.length-1;
-		while(2*i<=n)
-		{
-			int j=2*i;
-			if(j<n&&less(a,j+1,j))
-				j++;
-			if(!less(a,i,j))
-				{
-				
-				return false;}
-			else
-			{
-			//exch(k,j);
-			i=j;
-			 //z=false;
-			}
-		}
-		return true;
-	}
-	boolean sink1(int k,Double[] d)
-	{
-		int n=d.length-1;
-		while(2*k<=n)
-		{
-			int j=2*k;
-			if(j<n&&less1(d,j+1,j))
-				j++;
-			if(!less1(d,k,j))
-				{
-				
-				return false;}
-			else
-			{
-			//exch(k,j);
-			k=j;
-			 //z=false;
-			}
-		}
-		return true;
-	}
-	private boolean less(String[] a,int j, int i) {
-		
 		return a[j].compareTo(a[i])<=0;
 	}
-	
-	private boolean less1(Double[] d,int j, int i) {
-		//System.out.println(d.length+" "+j+" "+i);
-		return d[j]<=d[i];
+	boolean min(int i,String a[])
+	{
+		int n=a.length-1;
+		
+		while(2*i+1<n)
+		{
+			int j=2*i+1;
+			if(j<n && less(a,j+1,j))
+				j++;
+			if(!less(a,i,j))
+				return false;
+			else
+				i=j;
+		}
+		return true;
+		
 	}
+    boolean less1(Double[] d, int j, int i) 
+	{
+    	//System.out.println(d.length+" "+j+" "+i);
+		return d[j]<=d[i];
+		 
+	}
+	boolean minnumber(int i,Double[] d)
+	{
+		int n=d.length-1;
+		
+		while(2*i+1<n)
+		{
+			int j=2*i+1;
+			if(j<n && less1(d,j+1,j))
+				j++;
+			if(!less1(d,i,j))
+				return false;
+			else
+				i=j;
+		}
+		return true;
+		
+	}
+	
+}
+public class Solution 
+{
+	
+	public static void main(String[] args) 
+	{
+		 Scanner sc=new Scanner(System.in);
+		 String s=sc.next();
+		 switch(s)
+		 {
+		 case "String":
+			            int n=sc.nextInt();
+			            String s1[]=new String[n];
+			            String s2=sc.nextLine();
+			            for(int i=0;i<s1.length;i++)
+			            {
+			            	String s3=sc.next();
+			            	String s4[]=null;
+			            	s4=s3.split(",");
+			            	Heap h=new Heap();
+			            	System.out.println(h.min(0, s4));
+			            }
+			            break;
+		 case "Float":
+					 int i1=sc.nextInt();
+			            String[] s21=new String[i1];
+			            String in=sc.nextLine();
+			            for(int i=0;i<s21.length;i++)
+			            {
+			            	String s3=sc.next();
+			            	String s4[]=null;
+			            	s4=s3.split(",");
+			            Double d[]=new Double[s4.length+1];
+			          	for(int p=1;p<=s4.length;p++)
+			          	{
+			          		d[p]=Double.parseDouble(s4[p-1]);
+			          	}
+			            Heap h=new Heap();
+		         	System.out.println(h.minnumber(0,d));
+			            }
+			            break;
+		 case "Integer":
+					 int inte=sc.nextInt();
+			            String[] s31=new String[inte];
+			            String s11=sc.nextLine();
+			            for(int i=0;i<s31.length;i++)
+			            {
+			            	String s3=sc.next();
+			            	String s4[]=null;
+			            	s4=s3.split(",");
+			            Double d[]=new Double[s4.length+1];
+			          	for(int p=1;p<=s4.length;p++)
+			          	{
+			          		d[p]=Double.parseDouble(s4[p-1]);
+			          	}
+			            Heap h=new Heap();
+		         	System.out.println(h.minnumber(0,d));
+			            }
+			            break;
+		 case "Double":
+			            int i12=sc.nextInt();
+			            String[] sin=new String[i12];
+			            String i22=sc.nextLine();
+			            for(int i=0;i<sin.length;i++)
+			            {
+			            	String s3=sc.next();
+			            	String s4[]=null;
+			            	s4=s3.split(",");
+			            Double d[]=new Double[s4.length+1];
+			          	for(int p=1;p<=s4.length;p++)
+			          	{
+			          		d[p]=Double.parseDouble(s4[p-1]);
+			          	}
+			            Heap h=new Heap();
+		            	System.out.println(h.minnumber(0,d));
+			            }
+			            break;
+		 }
+
+	}
+
 }
